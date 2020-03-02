1f305d3f2d936dc0fd52c966fadb6720528f6f20
Titus Fortner <titusfortner@gmail.com>
2020-03-02 15:40:10 +0100

    ask forgiveness instead of permission

--- src/test/java/com/saucelabs/pages/BasePage.java
+++ src/test/java/com/saucelabs/pages/BasePage.java
@@ -1,6 +1,7 @@
 package test.java.com.saucelabs.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,13 +10,25 @@ public class BasePage {
     WebDriver driver;
 
     void click(By locator) {
-        waitForElement(locator);
-        driver.findElement(locator).click();
+        while (true) {
+            try {
+                driver.findElement(locator).click();
+                return;
+            } catch (NoSuchElementException e) {
+                waitForElement(locator);
+            }
+        }
     }
 
     void sendKeys(By locator, String text) {
-        waitForElement(locator);
-        driver.findElement(locator).sendKeys(text);
+        while (true) {
+            try {
+                driver.findElement(locator).sendKeys(text);
+                return;
+            } catch (NoSuchElementException e) {
+                waitForElement(locator);
+            }
+        }
     }
 
     private void waitForElement(By locator) {
